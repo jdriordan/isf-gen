698e51767d4b6220dfc7e6480f230191fe69ad08
Danny Riordan <dannyriordan@gmail.com>
2017-09-02 00:13:16 +0100

    need to fix case sensitivity

--- frink.rb
+++ frink.rb
@@ -6,6 +6,7 @@ search_phrase   = ARGV[0]
 replace_phrase  = ARGV[1]
 
 frame = Frinkiac::Screencap.search(search_phrase)[0]
+puts frame.caption
 new_caption = frame.caption.gsub(search_phrase,replace_phrase)
-
+puts new_caption
 puts frame.meme_url(new_caption)
